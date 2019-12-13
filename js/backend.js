56315ffd575e3e8a93bdb97ca87a50ec0de8e15b
Anton Liubimov <ant.liubimov@gmail.com>
2019-12-13 16:35:55 +0500

    done func load in js/backend.js

--- js/backend.js
+++ js/backend.js
@@ -2,5 +2,47 @@
 'use strict';
 
 (function() {
+  const URL = 'https://js.dump.academy/keksobooking';
+  /**
+   * Loading data from URL
+   * @param {callback} onLoad
+   * @param {callback} onError
+   */
+  const load = (onLoad, onError) => {
+    const xhr = XMLHttpRequest();
+    xhr.responseType = 'json';
+    const urlGet = `${URL}/data`;
 
+    xhr.addEventListener('load', function() {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError(`Response's status: ${xhr.status} ${xhr.statusText}`);
+      }
+    });
+
+    xhr.addEventListener('error', function() {
+      onError('Connection error occurred');
+    });
+    xhr.addEventListener('timeout', function() {
+      onError(`The request did not succeed in ${xhr.timeout} ms`);
+    });
+    xhr.timeout = 10000;
+
+    xhr.open('GET', urlGet);
+    xhr.send();
+  };
+
+  /**
+   * Send data to URL
+   * @param {data} data
+   * @param {callback} onLoad
+   * @param {callback} onError
+   */
+  const save = (data, onLoad, onError) => {};
+
+  window.backend = {
+    load,
+    save
+  };
 })();
